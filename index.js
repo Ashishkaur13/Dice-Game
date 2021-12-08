712a9f99ddcd4b852d4d63e4e4e7327c6dbf8390
Ashishkaur13 <71460195+Ashishkaur13@users.noreply.github.com>
2021-12-08 15:35:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+alert("HI, Welcome to the dice game! Decide you are player 1 or 2 and let the game begin!")
+
+var RandomNumber1 = Math.floor(Math.random() * 6 ) + 1 ;
+
+var DiceNumber = "Dice" + RandomNumber1 + ".png"; //dice1.png - dice6.png
+
+var ImageSource = "images/" + DiceNumber;
+
+var Image1 = document.querySelectorAll("img")[0];
+
+Image1.setAttribute("src", ImageSource);  //1st number is randomised
+
+var RandomNumber2 = Math.floor(Math.random() * 6 ) + 1;  //1-6 
+
+var Imagesource2 = "images/dice" + RandomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",Imagesource2);
+
+
+if (RandomNumber1 > RandomNumber2) 
+{
+document.querySelector("h1").innerHTML = "🚩 Player 1 wins" ;
+}
+else if (RandomNumber1 < RandomNumber2) 
+{
+document.querySelector("h1").innerHTML = "Player 2 wins🚩" ;
+}
+else if (RandomNumber1 = RandomNumber2) 
+{
+document.querySelector("h1").innerHTML = "Its a Draw!" ;
+}
